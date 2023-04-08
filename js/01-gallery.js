29fd9041af75b12caf1c0e37febb4b8540fb2840
Tatyana Hodakovskaya <115928261+Tatyanahod@users.noreply.github.com>
2023-04-08 20:25:51 +0300

    перед проверкой

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,45 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+
+galleryRef.insertAdjacentHTML(
+  'beforeend',
+  galleryItems
+    .map(
+      galleryItem =>
+        `<div class="gallery__item">
+        <a clss="gallery__link" href="${galleryItem.original}">
+        <img class="gallery__image"
+        src="${galleryItem.preview}" 
+        data-source="${galleryItem.original}"
+        alt= "${galleryItem.description}"></a></div>`
+    )
+    .join('')
+);
+
+galleryRef.addEventListener('click', activateOriginalImage);
+
+function activateOriginalImage(event) {
+  event.preventDefault();
+
+  console.log('activateOriginalImage -> target', event.target);
+  console.log('activateOriginalImage -> currentTarget', event.currentTarget);
+
+  console.log(event);
+  console.log(event.target.nodeName);
+
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+ 
+
+  console.log(event.target.dataset);
+  console.log(event.target.dataset.source);
+
+  const instance = basicLightbox.create(`<img src="${event.target.dataset.source}">`);
+  instance.show();
+
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector(".gallery");
+
+const markup = galleryItems
+	.map(
+		({ preview, original, description }) => `
+<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`,
+	)
+	.join("");
+
+galleryRef.insertAdjacentHTML("beforeend", markup);
+
+
+galleryRef.addEventListener("click", onGalleryClick);
+
+function onGalleryClick(evt) {
+	evt.preventDefault();
+}
+
+const lightbox = new SimpleLightbox(".gallery a", { captionsData: `alt`, captionDelay: `250` });
